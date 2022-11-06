c0eb0c7634fdce47dc463c75d4bc693af320bd7b
Plimeor <plimeor@gmail.com>
2022-11-06 22:57:34 +0800

    chore: init project

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.plimeor.flint',
+  appName: 'Flint',
+  webDir: './dist/public',
+  bundledWebRuntime: false
+};
+
+export default config;
